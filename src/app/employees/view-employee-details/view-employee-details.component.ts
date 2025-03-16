9752f88a2b6ae07f9983c24142e49384e84df9ea
Dominik Tarkiewicz <dominik.tarkiewicz@redge.com>
2025-03-16 23:25:27 +0100

    Style details page

--- src/app/employees/view-employee-details/view-employee-details.component.ts
+++ src/app/employees/view-employee-details/view-employee-details.component.ts
@@ -3,11 +3,13 @@ import { Employee, EmployeesService } from '../employees.service';
 import { MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
 import { OffboardingDialogComponent } from '../offboarding-dialog/offboarding-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-view-employee-details',
-  imports: [MatTableModule, RouterLink, OffboardingDialogComponent],
+  imports: [MatTableModule, RouterLink, OffboardingDialogComponent, MatButtonModule, MatIconModule],
   templateUrl: './view-employee-details.component.html',
   styleUrl: './view-employee-details.component.scss'
 })
