c00cf67266624e274b982e9795877b30e9d010f6
Dominik Tarkiewicz <dominik.tarkiewicz@redge.com>
2025-03-16 17:47:49 +0100

    Basic employee details

--- src/app/employees/view-employee-details/view-employee-details.component.ts
+++ src/app/employees/view-employee-details/view-employee-details.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee, EmployeesService } from '../employees.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-employee-details',
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, RouterLink],
   templateUrl: './view-employee-details.component.html',
   styleUrl: './view-employee-details.component.scss'
 })
-export class ViewEmployeeDetailsComponent {
+export class ViewEmployeeDetailsComponent implements OnInit {
+  employee?: Employee;
+  constructor(private employeesService: EmployeesService, private route: ActivatedRoute) {
 
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.employeesService.getEmployeeById(id!).subscribe(emp => {
+      this.employee = emp;
+    });
+  }
+
+  offboardEmployee(event: Event) {
+    alert('Offboarding employee with id: ' + event);
+  }
 }
