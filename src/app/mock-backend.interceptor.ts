8264f9d91456cfabeab69a9d63c5024ad1830271
Dominik Tarkiewicz <dominik.tarkiewicz@redge.com>
2025-03-16 21:07:11 +0100

    Mock server state

--- src/app/mock-backend.interceptor.ts
+++ src/app/mock-backend.interceptor.ts
@@ -14,19 +14,24 @@ import { Employee } from './employees/employees.service';
 
 @Injectable()
 export class MockBackendInterceptor implements HttpInterceptor {
+  mockServerState = employeesData;
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log(`%cSimulating %c${request.method} %crequest to %c${request.url}`, 'font-size: 1rem;', 'color: #fbba72; font-size: 1rem;', 'font-size: 1rem;', 'color: #fbba72; font-size: 1rem;');
 
     if (request.url.endsWith('/employees') && request.method === 'GET') {
-      return of(new HttpResponse({ status: 200, body: employeesData })).pipe(delay(500));
+      return of(new HttpResponse({ status: 200, body: this.mockServerState })).pipe(delay(500));
     }
 
     if (request.url.match(/\/employees\/.+$/) && request.method === 'GET') {
-      const employee = (employeesData as Employee[]).find(e => e.id === request.url.split('/').pop());
+      const employee = (this.mockServerState as Employee[]).find(e => e.id === request.url.split('/').pop());
       return of(new HttpResponse({ status: 200, body: employee })).pipe(delay(500));
     }
 
     if (request.url.match(/\/users\/.+\/offboard$/) && request.method === 'POST') {
+      this.mockServerState = this.mockServerState.map(emp =>
+        emp.id === request.url.split('/')[2] ? { ...emp, status: 'OFFBOARDED' } : emp
+      );
+
       return of(new HttpResponse({ status: 200, body: request.body })).pipe(delay(500));
     }
 
